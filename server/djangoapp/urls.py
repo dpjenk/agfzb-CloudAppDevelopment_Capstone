77516068b0a5cf23e639edab0ab54979bf71790f
dpjenk <55035421+dpjenk@users.noreply.github.com>
2023-01-03 15:55:12 -0600

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path('index/', views.get_dealerships, name='index'),
+    path(route='', view=views.get_dealerships, name='index'),
     # path for about view
-    path('about/', views.about, name='about'),
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
-    path('contact/', views.contact, name='contact'),
+    path(route='contact/', view=views.contact, name='contact'),
     # path for registration
     path('registration/', views.registration_request, name='registration'),
     # path for login
